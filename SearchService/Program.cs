0f0287b1387f93768422739290511faafea43c9d
ngochuy9123 <3121560037@sv.sgu.edu.vn>
2025-01-14 21:58:01 +0700

    finish search service

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,3 +1,5 @@
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -6,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,13 +29,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-	await DbInitializer.InitBd(app);
-}
-catch (Exception e)
-{
-	Console.WriteLine(e);
-}
+	try
+	{
+		await DbInitializer.InitBd(app);
+	}
+	catch (Exception e)
+	{
+		Console.WriteLine(e);
+	}
+
+});
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+	=> HttpPolicyExtensions.HandleTransientHttpError()
+		.OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+		.WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
