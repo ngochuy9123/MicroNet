fecbe1e0c4bf4dc5a93a60decbd2156bfbc959d1
ngochuy9123 <3121560037@sv.sgu.edu.vn>
2025-01-14 15:23:28 +0700

    add

--- AuctionServices/Controllers/AuctionsController.cs
+++ AuctionServices/Controllers/AuctionsController.cs
@@ -2,6 +2,7 @@
 using AuctionServices.DTOs;
 using AuctionServices.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,11 +22,16 @@ namespace AuctionServices.Controllers
 			_mapper = mapper;
 		}
 		[HttpGet]
-		public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+		public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string? date)
 		{
+			var query  = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+			if (!string.IsNullOrEmpty(date))
+			{
+				query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) >0);
+			}
 			var auctions = await _context.Auctions.Include(x => x.Item).OrderBy(x => x.Id).ToListAsync();
 
-			return _mapper.Map<List<AuctionDto>>(auctions);
+			return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
 		}
 		[HttpGet("{id}")]
 		public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
